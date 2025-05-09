342ad7ff55f7c218582a72462a89f87fb7dc2a62
Azrull <azroulalby69@gmail.com>
2025-05-10 01:04:49 +0700

    a

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,41 @@
 import { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://www.ambulan24jammagelang.my.id'
+  
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/admin/'],
-    },
-    sitemap: 'https://www.ambulan24jammagelang.my.id/sitemap.xml',
+    rules: [
+      {
+        userAgent: '*',
+        allow: [
+          '/',
+          '/*.html',
+          '/sitemap.xml',
+          '/#about',
+          '/#services',
+          '/#contact',
+          '/#testimonials',
+          '/#gallery'
+        ],
+        disallow: [
+          '/private/',
+          '/admin/',
+          '/*.json$',
+          '/*?*'
+        ]
+      },
+      {
+        userAgent: 'Googlebot',
+        allow: '/',
+        crawlDelay: 1
+      },
+      {
+        userAgent: 'Bingbot',
+        allow: '/',
+        crawlDelay: 1
+      }
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl
   }
 } 
