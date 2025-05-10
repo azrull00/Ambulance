cc9bf35d708cefca63dc44737c8db16a13f556eb
Azrull <azroulalby69@gmail.com>
2025-05-10 15:29:10 +0700

    fix sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,19 @@
-import { MetadataRoute } from 'next'
+import { NextResponse } from 'next/server'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://ambulan24jammagelang.my.id'
+export async function GET() {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://ambulan24jammagelang.my.id/</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+      <changefreq>daily</changefreq>
+      <priority>1.0</priority>
+    </url>
+  </urlset>`
 
-  return [
-    {
-      url: baseUrl,
-      lastModified: new Date(),
-      changeFrequency: 'daily',
-      priority: 1,
-    }
-  ]
+  return new NextResponse(body, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  })
 }
